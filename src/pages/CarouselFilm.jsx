64dc0c603549183b0bfecf52006ef3a9fec8822b
Hugo-rvs31 <hug.rvs31@gmail.com>
2025-11-21 18:55:40 +0100

    Ajouts d'élements sur CarouselFilm

--- src/pages/CarouselFilm.jsx
+++ src/pages/CarouselFilm.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 
 const CarouselFilm = () => {
   const [films, setFilms] = useState([]);
+  const [activeFilmId, setActiveFilmId] = useState(null);
 
   useEffect(() => {
     axios.get("/db-cinema.json").then((response) => {
@@ -17,14 +18,36 @@ const CarouselFilm = () => {
 
       <div className="inner-carousel-film">
         <div className="box-carousel-films">
-          {films.map((film) => (
-            <div className="each-film" key={film.id}>
-              <img src={film.image} alt={film.title} />
-              <div className="overlay">
-                <h3>{film.title}</h3>
+          {films.map((film) => {
+            const isActive = activeFilmId === film.id;
+
+            return (
+              <div
+                key={film.id}
+                className={isActive ? "each-film-expanded" : "each-film"}
+                onClick={() => setActiveFilmId(isActive ? null : film.id)}
+              >
+                <img
+                  className={isActive ? "image-film-expanded" : "image-film"}
+                  src={film.image}
+                  alt={film.title}
+                />
+                <div
+                  className={
+                    isActive ? "overlay-film-expanded" : "overlay-film"
+                  }
+                >
+                  <h3>{film.title}</h3>
+                  {isActive && (
+                    <p>
+                      Ce film est sorti en {film.annee} et a été réalisé par{" "}
+                      {film.realisateur}
+                    </p>
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </div>
