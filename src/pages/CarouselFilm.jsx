6fce7f5e5acff7975cb0303ff6265d13a9050fc8
Hugo-rvs31 <hug.rvs31@gmail.com>
2025-11-21 15:52:48 +0100

    Ajout éléments sur CarouselFilm

--- src/pages/CarouselFilm.jsx
+++ src/pages/CarouselFilm.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navigation from "../components/Navigation";
+import axios from "axios";
 
 const CarouselFilm = () => {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    axios.get("/db-cinema.json").then((response) => {
+      setFilms(response.data.films);
+    });
+  }, []);
+
   return (
     <div className="carousel-film">
       <Navigation />
+
+      <div className="inner-carousel-film">
+        <div className="box-carousel-films">
+          {films.map((film) => (
+            <div className="each-film" key={film.id}>
+              <img src={film.image} alt={film.title} />
+              <div className="overlay">
+                <h3>{film.title}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
